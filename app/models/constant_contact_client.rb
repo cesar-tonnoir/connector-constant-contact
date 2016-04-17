b074e408ee56aba87ef1d23034023ae76434ec3f
Pierre Berard <pierre.berard@maestrano.com>
2016-04-17 23:49:46 +0100

    Improves client

--- app/models/constant_contact_client.rb
+++ app/models/constant_contact_client.rb
@@ -10,68 +10,60 @@ class ConstantContactClient
     @headers["Content-Type"] = "application/json"
   end
 
-  def all(external_entity_name, modified_since=nil)
-    arr = []
-    url = get_entity_url(external_entity_name)
-    if modified_since == nil
-      data = self.class.get("#{url}?api_key=#{@api_key}", :headers => @headers)
-    else
-      data = self.class.get("#{url}?api_key=#{@api_key}&modified_since=#{modified_since}", :headers => @headers)
+  def all(entity_name, singleton?, modified_since=nil)
+    entity_params = get_entity_params(entity_name)
+    query_params = entity_params[:query_params] || {}
+    query_params.merge!(api_key: @api_key)
+    if modified_since
+      query_params.merge!(modified_since: modified_since.iso8601)
     end
-    external_entity_name == "Account" ? arr << data : data['results']
+    data = self.class.get("#{entity_params[:endpoint]}?query_params.to_query", :headers => @headers)
+    
+    singleton? ? [data] : data['results']
   end
 
-  def create(external_entity_name, entity)
-    url = get_entity_url(external_entity_name)
-    if url == "/eventspot/events"
-      modified_entity = modify_entity_for_event_creation(entity)
-      self.class.post("#{url}?api_key=#{@api_key}", :headers => @headers, :body => modified_entity.to_json)
-    else
-      self.class.post("#{url}?api_key=#{@api_key}", :headers => @headers, :body => entity.to_json)
-    end
+  def create(entity_name, entity)
+    endpoint = get_entity_params(entity_name)[:endpoint]
+    self.class.post("#{url}?api_key=#{@api_key}", :headers => @headers, :body => entity.to_json)
   end
 
-  def update(external_entity_name, entity, id)
-    url = get_entity_url(external_entity_name)
+  def update(entity_name, entity, id)
+    endpoint = get_entity_params(entity_name)[:endpoint]
     self.class.put("#{url}/#{id}?api_key=#{@api_key}", :headers => @headers, :body => entity.to_json)
   end
-
-  def modify_entity_for_event_creation(entity)
-    grp_id = Maestrano::Connector::Rails::Organization.first.uid
-    maestrano_client = Maestrano::Connec::Client.new(grp_id)
-    venue_detail = maestrano_client.get("/venues/#{entity[:venue_id]}")["venues"]
-    entity[:type] = "OTHER"
-    entity[:name] = venue_detail["name"]
-    entity[:title] = venue_detail["name"]
-    entity[:location] = venue_detail["name"]
-    entity[:time_zone_id] = "US/Eastern"
-    entity[:address] = {}
-    entity[:address][:city] = venue_detail["address"]["city"]
-    entity[:address][:state] = venue_detail["address"]["region"]
-    entity[:address][:country] = venue_detail["address"]["region"]
-    entity[:address][:line1] = venue_detail["address"]["line1"]
-    entity[:address][:line2] = venue_detail["address"]["line2"]
-    entity[:address][:state_code] = ""
-    entity[:address][:country_code] = ""
-    entity[:address][:postal_code] = venue_detail["address"]["postal_code"]
-    entity[:contact] = {}
-    entity[:contact][:name] = Maestrano::Connector::Rails::Organization.first.oauth_uid
-    entity[:contact][:organization_name] = Maestrano::Connector::Rails::Organization.first.name
-    entity.delete(:venue_id)
-    entity
-  end
+  # def modify_entity_for_event_creation(entity)
+  #   grp_id = Maestrano::Connector::Rails::Organization.first.uid
+  #   maestrano_client = Maestrano::Connec::Client.new(grp_id)
+  #   venue_detail = maestrano_client.get("/venues/#{entity[:venue_id]}")["venues"]
+  #   entity[:type] = "OTHER"
+  #   entity[:name] = venue_detail["name"]
+  #   entity[:title] = venue_detail["name"]
+  #   entity[:location] = venue_detail["name"]
+  #   entity[:time_zone_id] = "US/Eastern"
+  #   entity[:address] = {}
+  #   entity[:address][:city] = venue_detail["address"]["city"]
+  #   entity[:address][:state] = venue_detail["address"]["region"]
+  #   entity[:address][:country] = venue_detail["address"]["region"]
+  #   entity[:address][:line1] = venue_detail["address"]["line1"]
+  #   entity[:address][:line2] = venue_detail["address"]["line2"]
+  #   entity[:address][:state_code] = ""
+  #   entity[:address][:country_code] = ""
+  #   entity[:address][:postal_code] = venue_detail["address"]["postal_code"]
+  #   entity[:contact] = {}
+  #   entity[:contact][:name] = Maestrano::Connector::Rails::Organization.first.oauth_uid
+  #   entity[:contact][:organization_name] = Maestrano::Connector::Rails::Organization.first.name
+  #   entity.delete(:venue_id)
+  #   entity
+  # end
 
   private
-    def get_entity_url(external_entity_name)
-      name = external_entity_name.downcase.pluralize
-      case name
-        when "contacts"
-          "/contacts"
-        when "events"
-          "/eventspot/events"
-        else
-          "/account/info"
-        end
+    def get_entity_params(entity_name)
+      {
+        'Contact' => {endpoint: '/contacts', query_params: {status: 'ACTIVE'}}, 
+        'Event' => {endpoint: '/eventspot/events'}, 
+        'Account' => {endpoint: '/account/info'}, 
+        'List' => {endpoint: '/lists'}, 
+      }[entity_name]
     end
 
 end
--- app/models/maestrano/connector/rails/external.rb
+++ app/models/maestrano/connector/rails/external.rb
@@ -2,11 +2,10 @@ class Maestrano::Connector::Rails::External
   include Maestrano::Connector::Rails::Concerns::External
 
   def self.external_name
-   'ConstantContact'
+   'Constant Contact'
   end
 
   def self.get_client(organization)
-    # Create New Client
     token = "Bearer " + organization.oauth_token
     ConstantContactClient.new(ENV[:constantcontact_key], token)
   end
